b8751f0a8d72183e6fbe9fc529d4524cda556070
kmsync <tommyjwi@gmail.com>
2021-12-26 10:55:31 +0300

    Polymorphism

---
+++ src/com/bilgeadam/yhb/Esek.java
@@ -0,0 +1,10 @@
+package com.bilgeadam.yhb;
+
+public class Esek extends Hayvan{
+
+    public void sesCikar()
+    {
+        System.out.println("Aaaaa iiiiii");
+    }
+
+}
---
+++ src/com/bilgeadam/yhb/Kedi.java
@@ -0,0 +1,10 @@
+package com.bilgeadam.yhb;
+
+public class Kedi extends Hayvan {
+
+    public void sesCikar()
+    {
+        System.out.println("Miuw Miuw");
+    }
+
+}
